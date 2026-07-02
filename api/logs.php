423042736cfbb0cd2822f4d1bc84ad36d50f37f4
khanhduygithub <truongnguyenkhanhduy2010pr@gmail.com>
2026-07-02 20:12:41 +0700

    Delete api directory

--- api/logs.php
+++
@@ -1,8 +0,0 @@
-<?php
-// api/logs.php
-
-$db = initDB();
-$result = $db->query('SELECT * FROM logs ORDER BY timestamp DESC LIMIT 100');
-$logs = [];
-while ($row = $result->fetchArray(SQLITE3_ASSOC)) $logs[] = $row;
-json_response(['success' => true, 'logs' => $logs]);
